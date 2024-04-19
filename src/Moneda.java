644f88ff9f0520024e0cdb907e77227ce5cbb5a4
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2024-04-19 13:44:13 -0500

    Delete src directory

--- src/Moneda.java
+++
@@ -1,9 +0,0 @@
-public record Moneda(String base_code,
-                     String target_code,
-                       double conversion_result) {
-
-    @Override//aparece cuando esta sobre-escribiendo un metodo y en este caso es el "toString"
-    public String toString() {
-        return "["+base_code+"] corresponde al valor final de =>> " + conversion_result +" ["+target_code+"]";
-    }
-}
